34adcd80d1b5d958e0491bb171c6e4b4beb99191
unknown <aruru03102644@gmail.com>
2026-01-06 23:06:25 +0900

    [Feat] Revise Byte Enable Logic Partial byte level Load logics and refactored codes

--- modules/Byte_Enable_Logic.v
+++ modules/Byte_Enable_Logic.v
@@ -23,29 +23,59 @@ module ByteEnableLogic #(
 			
 			case (funct3)
 				`LOAD_LB: begin
-					register_file_write_data = {{(XLEN-8){data_memory_read_data[7]}}, data_memory_read_data[7:0]};
-				end
+                case (address[2:0])
+                    3'b000: register_file_write_data = {{56{data_memory_read_data[7]}},  data_memory_read_data[7:0]};
+                    3'b001: register_file_write_data = {{56{data_memory_read_data[15]}}, data_memory_read_data[15:8]};
+                    3'b010: register_file_write_data = {{56{data_memory_read_data[23]}}, data_memory_read_data[23:16]};
+                    3'b011: register_file_write_data = {{56{data_memory_read_data[31]}}, data_memory_read_data[31:24]};
+                    3'b100: register_file_write_data = {{56{data_memory_read_data[39]}}, data_memory_read_data[39:32]};
+                    3'b101: register_file_write_data = {{56{data_memory_read_data[47]}}, data_memory_read_data[47:40]};
+                    3'b110: register_file_write_data = {{56{data_memory_read_data[55]}}, data_memory_read_data[55:48]};
+                    3'b111: register_file_write_data = {{56{data_memory_read_data[63]}}, data_memory_read_data[63:56]};
+                endcase
+            end
 				`LOAD_LH: begin
-					register_file_write_data = {{(XLEN-16){data_memory_read_data[15]}}, data_memory_read_data[15:0]};
-				end
+                case (address[2:1])
+                    2'b00: register_file_write_data = {{48{data_memory_read_data[15]}}, data_memory_read_data[15:0]};
+                    2'b01: register_file_write_data = {{48{data_memory_read_data[31]}}, data_memory_read_data[31:16]};
+                    2'b10: register_file_write_data = {{48{data_memory_read_data[47]}}, data_memory_read_data[47:32]};
+                    2'b11: register_file_write_data = {{48{data_memory_read_data[63]}}, data_memory_read_data[63:48]};
+                endcase
+            end
 				`LOAD_LW: begin
-					register_file_write_data = {{(XLEN-32){data_memory_read_data[31]}}, data_memory_read_data[31:0]};
-				end
-                `LOAD_LD: begin
-					register_file_write_data = data_memory_read_data;
-                end
+                case (address[2])
+                    1'b0: register_file_write_data = {{32{data_memory_read_data[31]}}, data_memory_read_data[31:0]};
+                    1'b1: register_file_write_data = {{32{data_memory_read_data[63]}}, data_memory_read_data[63:32]};
+                endcase
+            end
+				`LOAD_LD: register_file_write_data = data_memory_read_data;
 				`LOAD_LBU: begin
-					register_file_write_data = {{(XLEN-8){1'b0}}, data_memory_read_data[7:0]};
-				end
+                case (address[2:0])
+                    3'b000: register_file_write_data = {56'b0, data_memory_read_data[7:0]};
+                    3'b001: register_file_write_data = {56'b0, data_memory_read_data[15:8]};
+                    3'b010: register_file_write_data = {56'b0, data_memory_read_data[23:16]};
+                    3'b011: register_file_write_data = {56'b0, data_memory_read_data[31:24]};
+                    3'b100: register_file_write_data = {56'b0, data_memory_read_data[39:32]};
+                    3'b101: register_file_write_data = {56'b0, data_memory_read_data[47:40]};
+                    3'b110: register_file_write_data = {56'b0, data_memory_read_data[55:48]};
+                    3'b111: register_file_write_data = {56'b0, data_memory_read_data[63:56]};
+                endcase
+            end
 				`LOAD_LHU: begin
-					register_file_write_data = {{(XLEN-16){1'b0}}, data_memory_read_data[15:0]};
-				end
+                case (address[2:1])
+                    2'b00: register_file_write_data = {48'b0, data_memory_read_data[15:0]};
+                    2'b01: register_file_write_data = {48'b0, data_memory_read_data[31:16]};
+                    2'b10: register_file_write_data = {48'b0, data_memory_read_data[47:32]};
+                    2'b11: register_file_write_data = {48'b0, data_memory_read_data[63:48]};
+                endcase
+            end
                 `LOAD_LWU: begin
-					register_file_write_data = {{(XLEN-32){1'b0}}, data_memory_read_data[31:0]};
-                end
-				default: begin
-					register_file_write_data = {XLEN{1'b0}};
-				end
+                case (address[2])
+                    1'b0: register_file_write_data = {32'b0, data_memory_read_data[31:0]};
+                    1'b1: register_file_write_data = {32'b0, data_memory_read_data[63:32]};
+                endcase
+            end
+				default: register_file_write_data = {XLEN{1'b0}};
 			endcase
 		end
 		else if (memory_write) begin
@@ -56,66 +86,34 @@ module ByteEnableLogic #(
 					data_memory_write_data = {8{register_file_read_data[7:0]}};
 					
 					case (address[2:0])
-						3'b000: begin
-							write_mask = 8'b0000_0001;
-						end
-						3'b001: begin
-							write_mask = 8'b0000_0010;
-						end
-						3'b010: begin
-							write_mask = 8'b0000_0100;
-						end
-						3'b011: begin
-							write_mask = 8'b0000_1000;
-						end
-						3'b100: begin
-							write_mask = 8'b0001_0000;
-						end
-						3'b101: begin
-							write_mask = 8'b0010_0000;
-						end
-						3'b110: begin
-							write_mask = 8'b0100_0000;
-						end
-						3'b111: begin
-							write_mask = 8'b1000_0000;
-						end
+						3'b000: write_mask = 8'b0000_0001;
+						3'b001: write_mask = 8'b0000_0010;
+						3'b010: write_mask = 8'b0000_0100;
+						3'b011: write_mask = 8'b0000_1000;
+						3'b100: write_mask = 8'b0001_0000;
+						3'b101: write_mask = 8'b0010_0000;
+						3'b110: write_mask = 8'b0100_0000;
+						3'b111: write_mask = 8'b1000_0000;
 					endcase
 				end
 				`STORE_SH: begin
 					data_memory_write_data = {4{register_file_read_data[15:0]}};
 					
 					case (address[2:0])
-						3'b000: begin
-							write_mask = 8'b0000_0011;
-						end
-						3'b010: begin
-							write_mask = 8'b0000_1100;
-						end
-						3'b100: begin
-							write_mask = 8'b0011_0000;
-						end
-						3'b110: begin
-							write_mask = 8'b1100_0000;
-						end
-						default: begin
-							write_mask = 8'b0;
-						end
+						3'b000: write_mask = 8'b0000_0011;
+						3'b010: write_mask = 8'b0000_1100;
+						3'b100: write_mask = 8'b0011_0000;
+						3'b110: write_mask = 8'b1100_0000;
+						default: write_mask = 8'b0;
 					endcase
 				end
 				`STORE_SW: begin
 					data_memory_write_data = {2{register_file_read_data[31:0]}};
 					
 					case (address[2:0])
-						3'b000: begin
-							write_mask = 8'b0000_1111;
-						end
-						3'b100: begin
-							write_mask = 8'b1111_0000;
-						end
-						default: begin
-							write_mask = 8'b0;
-						end
+						3'b000: write_mask = 8'b0000_1111;
+						3'b100: write_mask = 8'b1111_0000;
+						default: write_mask = 8'b0;
 					endcase
 				end
 				`STORE_SD: begin
